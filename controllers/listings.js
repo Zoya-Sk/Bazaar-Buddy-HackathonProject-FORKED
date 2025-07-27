c39551cd2ca750dbd7611c86996d723db42c9d5c
shifantasticc <shifastudyit@gmail.com>
2025-07-27 21:27:02 +0530

    Adding if review > 5 then Certified

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,7 +4,7 @@ const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
-  const allListings = await Listing.find({});
+  const allListings = await Listing.find({}).populate('reviews');
   const weatherApiKey = process.env.WEATHER_API_KEY;
   res.render('listings/index.ejs', { allListings, weatherApiKey });
 };
